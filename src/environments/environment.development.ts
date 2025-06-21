afd5edfe4472711f6211ef7fcda4a4d6e9c16bd3
pynhaXD <roma151205@mail.ru>
2025-06-21 15:43:15 +0700

    test ignore

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  api_url: 'https://6d8a39e70ba8753e.mokky.dev',
-  gemini_api_url: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=',
-};
-//test
